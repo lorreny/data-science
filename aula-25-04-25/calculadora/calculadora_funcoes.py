0896bd982fa4030567f6dde0e9d1d2c64f6ff0e0
Lorreny <38407983+lorreny@users.noreply.github.com>
2025-04-25 21:04:49 -0300

    Create calculadora_funcoes.py

---
+++ aula-25-04-25/calculadora/calculadora_funcoes.py
@@ -0,0 +1,20 @@
+# calculadora_funcoes.py
+
+# Função para somar
+def add(x, y):
+    return x + y
+
+# Função para subtrair
+def subtract(x, y):
+    return x - y
+
+# Função para multiplicar
+def multiply(x, y):
+    return x * y
+
+# Função para dividir
+def divide(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Erro! Divisão por zero."
